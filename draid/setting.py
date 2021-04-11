b50f49e0275dcf7d17dcf9fc9cd3d7a1831ff6eb
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2021-04-11 21:53:04 +0100

    Remove testing rules

--- draid/setting.py
+++ draid/setting.py
@@ -35,16 +35,16 @@ INJECTED_FLOW_RULE = {
         ###### k1 : {k2 : v}  <==> graph_id : {component_name/function: rule_str}
         None : {
             # k2 : v  <==>  component_name/function: rule_str
-            'NumberProducer': '',
-            'Increase': '',
-            'Redispatch': '''{'output0': ['input0', 'input1'], 'output1': ['input0', 'input2'], 'output3': ['input1', 'input2']}''',
+            # 'NumberProducer': '',
+            # 'Increase': '',
+            # 'Redispatch': '''{'output0': ['input0', 'input1'], 'output1': ['input0', 'input2'], 'output3': ['input1', 'input2']}''',
             # 'arcp://uuid,19bb7653-72fd-4a80-8e4b-44d409346434/workflow/packed.cwl#main/processfiles_2': rr.Remove_Source_UoE,
             # 'arcp://uuid,19bb7653-72fd-4a80-8e4b-44d409346434/workflow/packed.cwl#main/processfiles_3': rr.Change_Source_UoE_UK,
             },
-        URIRef('http://schema.org#2a3c188f8cd6-19-b9fee0c2-7179-11e9-bd29-0242ac120003') : {
-            # k1 : {k2 : v}  <==>  graph_id : {component_id: rule_str}
-            URIRef('123'): '{}',
-            }
+        # URIRef('http://schema.org#2a3c188f8cd6-19-b9fee0c2-7179-11e9-bd29-0242ac120003') : {
+        #     # k1 : {k2 : v}  <==>  graph_id : {component_id: rule_str}
+        #     URIRef('123'): '{}',
+        #     }
         }
 
 # The extra data rules to be injected into the reasoner, through the "import" port.
@@ -55,12 +55,12 @@ INJECTED_FLOW_RULE = {
 INJECTED_IMPORTED_RULE = {
         None: {
                 # k:v <==> component_name/function: rule_str
-                'Source' : None,
-                'downloadPE' : None,
-                'Collector' : None,
-                'COLLECTOR1' : None,
-                'COLLECTOR2' : None,
-                'NumberProducer' : None,
+                # 'Source' : None,
+                # 'downloadPE' : None,
+                # 'Collector' : None,
+                # 'COLLECTOR1' : None,
+                # 'COLLECTOR2' : None,
+                # 'NumberProducer' : None,
                 # 'arcp://uuid,19bb7653-72fd-4a80-8e4b-44d409346434/workflow/packed.cwl#main/create_environment' : rr.Acknowledgement_UoE,
                 # 'arcp://uuid,19bb7653-72fd-4a80-8e4b-44d409346434/workflow/packed.cwl#main/create_environment_2' : rr.Acknowledgement_UoE,
                 # 'arcp://uuid,19bb7653-72fd-4a80-8e4b-44d409346434/workflow/packed.cwl#main/create_environment_3' : rr.Acknowledgement_UoE,
